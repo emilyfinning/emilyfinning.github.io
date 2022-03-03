95ce1d4784899826725e5ea268db30c635ce72cb
emilyfinning <em@emilyjay.co.uk>
2022-03-03 18:44:10 +0000

    chore: aesthetic changes

--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -8,9 +8,21 @@ const HomePage: React.FC = () => {
   return (
     <Box>
       <Box className="header-box">
+        <Box className="dec-line-1" />
+        <Box className="dec-line-2" />
         <HeaderContent />
         <Navigation />
       </Box>
+      <img
+        src={require("../../Images/code3.jpg")}
+        alt="Some code"
+        className="code-pic"
+      />
+      <img
+        src={require("../../Images/leaf.jpg")}
+        alt="Some leaves"
+        className="leaf-pic"
+      />
       <Box className="about-box"></Box>
     </Box>
   );

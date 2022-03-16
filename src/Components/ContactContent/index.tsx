ec12bfd113fc06e61596984afbdf49defee53cb5
emilyfinning <em@emilyjay.co.uk>
2022-03-16 12:31:39 +0000

    feat: working contact form

--- src/Components/ContactContent/index.tsx
+++ src/Components/ContactContent/index.tsx
@@ -1,8 +1,57 @@
-import React from "react";
-import { Box, Typography, TextField, Button } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Typography, TextField, Button, FormControl } from "@mui/material";
+import validator from "validator";
+import emailjs from "@emailjs/browser";
 import "./styles.css";
+import _ from "lodash";
 
 const ContactContent: React.FC = () => {
+  const [message, setMessage] = useState<{
+    [key: string]: string;
+  }>({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [emailError, setEmailError] = useState(false);
+  const [attemptedSubmit, setAttemptedSubmit] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newMessage = _.cloneDeep(message);
+    newMessage[e.target.id] = e.target.value;
+    setMessage(newMessage);
+    if (e.target.id === "email") {
+      setEmailError(!validator.isEmail(e.target.value));
+    }
+  };
+
+  const sendMessage = () => {
+    setAttemptedSubmit(true);
+    if (
+      message.name !== "" &&
+      message.email !== "" &&
+      message.message !== "" &&
+      !emailError
+    ) {
+      emailjs
+        .sendForm(
+          process.env.REACT_APP_EMAILJS_SERVICE_ID!,
+          process.env.REACT_APP_EMAILJS_TEMPLATE!,
+          "#messageForm",
+          process.env.REACT_APP_EMAILJS_ACCESS_KEY
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    }
+  };
+
   return (
     <Box className="contact-container">
       <Typography variant="h2" className="contact-title">
@@ -10,28 +59,46 @@ const ContactContent: React.FC = () => {
       </Typography>
       <Box className="dec-line-contact" />
       <Box className="field-container">
-        <TextField
-          variant="standard"
-          label="Name"
-          fullWidth
-          sx={{ backgroundColor: "white", marginTop: "10px" }}
-        />
-        <TextField
-          variant="standard"
-          label="Email"
-          fullWidth
-          sx={{ backgroundColor: "white", marginTop: "10px" }}
-        />
-        <TextField
-          variant="standard"
-          label="Message"
-          fullWidth
-          sx={{ backgroundColor: "white", marginTop: "10px" }}
-          multiline
-          minRows={6}
-        />
+        <FormControl id="messageForm" component="form">
+          <TextField
+            variant="standard"
+            label="Name"
+            id="name"
+            name="name"
+            fullWidth
+            sx={{ backgroundColor: "white", marginTop: "10px" }}
+            onChange={handleChange}
+            error={attemptedSubmit && message.name === ""}
+          />
+          <TextField
+            variant="standard"
+            label="Email"
+            id="email"
+            name="email"
+            fullWidth
+            sx={{ backgroundColor: "white", marginTop: "10px" }}
+            onChange={handleChange}
+            error={attemptedSubmit && (message.email === "" || emailError)}
+          />
+          <TextField
+            variant="standard"
+            label="Message"
+            id="message"
+            name="message"
+            fullWidth
+            sx={{ backgroundColor: "white", marginTop: "10px" }}
+            multiline
+            minRows={6}
+            onChange={handleChange}
+            error={attemptedSubmit && message.message === ""}
+          />
+        </FormControl>
 
-        <Button variant="contained" sx={{ marginTop: "10px" }}>
+        <Button
+          variant="contained"
+          sx={{ marginTop: "10px" }}
+          onClick={sendMessage}
+        >
           Submit
         </Button>
       </Box>

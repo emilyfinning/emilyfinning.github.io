fb6fa8e5b3c42599f57a1dfd19d01dadfa9753a8
emilyfinning <em@emilyjay.co.uk>
2022-05-12 14:06:11 +0100

    about me responsiveness

--- src/Components/AboutContent/index.tsx
+++ src/Components/AboutContent/index.tsx
@@ -5,6 +5,9 @@ import "./styles.css";
 const AboutContent: React.FC = () => {
   return (
     <Box className="about-container">
+      <Typography variant="h1" className="mobile-title">
+        ABOUT ME
+      </Typography>
       <Typography variant="body2" className="desc-p1">
         I'm a recent graduate of Warwick University currently working in
         Security to deliver high quality front-end applications to our
@@ -24,50 +27,52 @@ const AboutContent: React.FC = () => {
         </Typography>
       </Box>
       <Box className="about-line-detail" />
-      <Box className="skills-box">
-        <img
-          src={require("../../Images/Icons/idea.png")}
-          className="skills-icon"
-          alt="lightbulb icon"
-        />
-        <Typography variant="h3">SKILLS</Typography>
-      </Box>
-      <Box className="skill-container">
-        <img
-          src={require("../../Images/Icons/atom.png")}
-          alt="React Icon"
-          className="skill-icon"
-        />
-        <img
-          src={require("../../Images/Icons/java-script.png")}
-          alt="Javascript Icon"
-          className="skill-icon"
-        />
-        <img
-          src={require("../../Images/Icons/html.png")}
-          alt="HTML Icon"
-          className="skill-icon"
-        />
-        <img
-          src={require("../../Images/Icons/files.png")}
-          alt="CSS Icon"
-          className="skill-icon"
-        />
-        <img
-          src={require("../../Images/Icons/git.png")}
-          alt="Git Icon"
-          className="skill-icon"
-        />
-        <img
-          src={require("../../Images/Icons/python.png")}
-          alt="Python Icon"
-          className="skill-icon"
-        />
-        <img
-          src={require("../../Images/Icons/java.png")}
-          alt="Java Icon"
-          className="skill-icon"
-        />
+      <Box className="skill-parent">
+        <Box className="skills-box">
+          <img
+            src={require("../../Images/Icons/idea.png")}
+            className="skills-icon"
+            alt="lightbulb icon"
+          />
+          <Typography variant="h3">SKILLS</Typography>
+        </Box>
+        <Box className="skill-container">
+          <img
+            src={require("../../Images/Icons/atom.png")}
+            alt="React Icon"
+            className="skill-icon"
+          />
+          <img
+            src={require("../../Images/Icons/java-script.png")}
+            alt="Javascript Icon"
+            className="skill-icon"
+          />
+          <img
+            src={require("../../Images/Icons/html.png")}
+            alt="HTML Icon"
+            className="skill-icon"
+          />
+          <img
+            src={require("../../Images/Icons/files.png")}
+            alt="CSS Icon"
+            className="skill-icon"
+          />
+          <img
+            src={require("../../Images/Icons/git.png")}
+            alt="Git Icon"
+            className="skill-icon"
+          />
+          <img
+            src={require("../../Images/Icons/python.png")}
+            alt="Python Icon"
+            className="skill-icon"
+          />
+          <img
+            src={require("../../Images/Icons/java.png")}
+            alt="Java Icon"
+            className="skill-icon"
+          />
+        </Box>
       </Box>
     </Box>
   );
